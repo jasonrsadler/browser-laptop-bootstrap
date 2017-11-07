fd1acd3e4e5c4115ed5edc74d722200c4d2c6d3c
Henrique Ferreiro <hferreiro@igalia.com>
2017-11-07 13:36:04 +0100

    Add a way to start brave in gdb
    'npm run debug' starts brave inside gdb.

--- scripts/commands.js
+++ scripts/commands.js
@@ -55,6 +55,7 @@ program
   .option('--user_data_dir_name [base_name]', 'set user data directory base name to [base_name]', 'brave-development')
   .option('--node_env [env]', 'set the node env to [env]', 'development')
   .option('--no_sandbox', 'disable the sandbox')
+  .option('--debug', 'run in debugger')
   .arguments('[build_config]')
   .action(start)
 
